e6e7b7288b302e83644252e0ee9ad2ebd21b5fd7
Sergii Mikhtoniuk <mikhtonyuk@gmail.com>
2013-12-21 18:24:17 +0200

    final backcomp

--- rx/executors/std_executor_wrapper.py
+++ rx/executors/std_executor_wrapper.py
@@ -4,12 +4,22 @@ from concurrent.futures import ProcessPoolExecutor as PPX
 
 
 class ThreadPoolExecutor(TPX):
-    def submit(self, fn, *args, **kwargs) -> Future:
+    def submit(self, fn, *args, **kwargs):
         cf = TPX.submit(self, fn, *args, **kwargs)
         return Future.from_concurrent_future(cf)
 
+    # assist type inference
+    def __enter__(self):
+        TPX.__enter__(self)
+        return self
+
 
 class ProcessPoolExecutor(PPX):
-    def submit(self, fn, *args, **kwargs) -> Future:
+    def submit(self, fn, *args, **kwargs):
         cf = PPX.submit(self, fn, *args, **kwargs)
         return Future.from_concurrent_future(cf)
+
+    # assist type inference
+    def __enter__(self):
+        PPX.__enter__(self)
+        return self
